56f46cb31fe923aa14dea47bf3d564a8ef99cb4c
huo <hsl_4444@163.com>
2016-07-01 13:24:46 +0800

    tijiao

--- app/src/main/java/com/example/huo/myappgit/ui/fragment/viewpager/PagerFragment2.java
+++ app/src/main/java/com/example/huo/myappgit/ui/fragment/viewpager/PagerFragment2.java
@@ -2,19 +2,32 @@ package com.example.huo.myappgit.ui.fragment.viewpager;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.huo.myappgit.R;
 import com.example.huo.myappgit.base.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link BaseFragment} subclass.
  */
 public class PagerFragment2 extends BaseFragment {
 
+
+    @BindView(R.id.ivBubble1)
+    ImageView mIvBubble1;
+    @BindView(R.id.ivBubble2)
+    ImageView mIvBubble2;
+    @BindView(R.id.ivBubble3)
+    ImageView mIvBubble3;
+    Animation mAnimation;
 
     public PagerFragment2() {
         // Required empty public constructor
@@ -25,7 +38,32 @@ public class PagerFragment2 extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pager_fragment2, container, false);
+        View view = inflater.inflate(R.layout.fragment_pager_fragment2, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        mIvBubble1.setVisibility(View.INVISIBLE);
+        mIvBubble2.setVisibility(View.INVISIBLE);
+        mIvBubble3.setVisibility(View.INVISIBLE);
+        mAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.translate);
+        showAnim();
+    }
+
+    private void showAnim() {
+        if (mIvBubble1.getVisibility() == View.INVISIBLE) {
+            mIvBubble1.setVisibility(View.VISIBLE);
+            mIvBubble1.setAnimation(mAnimation);
+        }
+        if (mIvBubble2.getVisibility() == View.INVISIBLE) {
+            mIvBubble2.setVisibility(View.VISIBLE);
+            mIvBubble2.setAnimation(mAnimation);
+        }
+        if (mIvBubble3.getVisibility() == View.INVISIBLE) {
+            mIvBubble3.setVisibility(View.VISIBLE);
+            mIvBubble3.setAnimation(mAnimation);
+        }
+    }
 }
